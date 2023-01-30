48199e97fc7b6d3fe1fecf832d2ec0ad1c46ff49
Damian Kurpiewski <blackbat13@gmail.com>
2023-01-30 18:01:20 +0100

    Modified game according to tutorial

--- main.py
+++ main.py
@@ -1,170 +1,147 @@
-import random
-
 import pgzrun
 import pygame
+import random
 
 
-""" CONFIGURATION """
+"""CONFIGURATION"""
 
 WIDTH = 600
 HEIGHT = 900
 
-""" VARIABLES """
+"""VARIABLES"""
 
-player = Actor("ship")
-player.x = WIDTH / 2
-player.y = HEIGHT - 60
-player.vx = 5
-player.lifes = 3
-player.ammo = 1
-player.points = 0
+ship = Actor("ship")
+ship.x = WIDTH / 2
+ship.y = HEIGHT - 60
+ship.vx = 5
+ship.points = 0
+ship.lifes = 3
+ship.ammunition = 5
 
-meteors_list = []
-
+asteroids_list = []
 lasers_list = []
 
-lifes_list = []
 
-
-""" DRAW """
+"""DRAW"""
 
 
 def draw():
     screen.blit("bg", (0, 0))
+    for asteroid in asteroids_list:
+        asteroid.draw()
 
-    draw_actors_list(meteors_list)
-    draw_actors_list(lasers_list)
-    draw_actors_list(lifes_list)
+    for laser in lasers_list:
+        laser.draw()
 
-    player.draw()
-
-    screen.draw.text(str(player.points), center=(
+    ship.draw()
+    screen.draw.text(str(ship.points), center=(
         WIDTH / 2, 20), fontsize=50, color="yellow")
-
-    if player.lifes == 0:
-        screen.draw.text("Game Over", center=(
-            WIDTH / 2, HEIGHT / 2), fontsize=90)
-
-
-def draw_actors_list(actors_list):
-    for actor in actors_list:
-        actor.draw()
+    draw_lifes()
+    if ship.lifes <= 0:
+        screen.draw.text("GAME OVER", center=(
+            WIDTH / 2, HEIGHT / 2), fontsize=90, color="yellow")
 
 
-""" UPDATE """
+def draw_lifes():
+    for life_id in range(1, ship.lifes + 1):
+        life = Actor("life")
+        life.x = life_id * life.width
+        life.y = life.height / 2
+        life.draw()
+
+
+"""UPDATE"""
 
 
 def update():
-    if player.lifes == 0:
+    if ship.lifes <= 0:
         return
 
-    if random.randint(0, 250) <= 1:
-        add_meteor()
-
-    update_player()
-    update_meteors()
-    update_lasers()
-    update_hits()
-
-
-def update_player():
     mouse_x, mouse_y = pygame.mouse.get_pos()
+    if mouse_x < ship.x:
+        ship.x -= ship.vx
 
-    if mouse_x < player.x:
-        player.x -= player.vx
+    if mouse_x > ship.x:
+        ship.x += ship.vx
 
-    if mouse_x > player.x:
-        player.x += player.vx
+    if random.random() < 0.02:
+        add_asteroid()
+
+    update_asteroids()
+    update_lasers()
+    update_lasers_hits()
 
 
-def update_meteors():
-    for meteor in meteors_list[:]:
-        meteor.y += meteor.vy
-        meteor.angle += meteor.va
-
-        if meteor.y > HEIGHT + 50:
-            meteors_list.remove(meteor)
-
-        if meteor.colliderect(player):
-            player.lifes -= 1
-            lifes_list.pop()
-            meteors_list.remove(meteor)
-            if player.lifes > 0:
+def update_asteroids():
+    for asteroid in asteroids_list[:]:
+        asteroid.y += asteroid.vy
+        if asteroid.y > HEIGHT + 50:
+            asteroids_list.remove(asteroid)
+        elif asteroid.colliderect(ship):
+            ship.lifes -= 1
+            asteroids_list.remove(asteroid)
+            if ship.lifes > 0:
                 sounds.shield.play()
             else:
                 sounds.explosion.play()
-                sounds.game_over.play()
-                music.fadeout(1)
 
 
 def update_lasers():
     for laser in lasers_list[:]:
         laser.y += laser.vy
-
         if laser.y < -laser.height:
             lasers_list.remove(laser)
 
 
-def update_hits():
+def update_lasers_hits():
     for laser in lasers_list[:]:
-        for meteor in meteors_list[:]:
-            if laser.colliderect(meteor):
+        for asteroid in asteroids_list[:]:
+            if laser.colliderect(asteroid):
                 lasers_list.remove(laser)
-                meteors_list.remove(meteor)
-                player.points += 10
-                sounds.hit.play()
-                return
+                asteroids_list.remove(asteroid)
+                ship.points += 1
+                break
 
 
-""" EVENTS """
+"""HELPERS"""
 
 
-def on_mouse_down():
-    if player.ammo > 0:
-        add_laser()
-        player.ammo -= 1
-        clock.schedule(add_ammo, 1)
-        sounds.laser.play()
-
-
-""" HELPERS """
+def add_asteroid():
+    image_id = random.randint(1, 4)
+    asteroid = Actor("asteroid" + str(image_id))
+    asteroid.x = random.randint(20, WIDTH-20)
+    asteroid.y = -10
+    asteroid.vy = random.randint(2, 10)
+    asteroids_list.append(asteroid)
 
 
 def add_laser():
     laser = Actor("laser")
-    laser.x = player.x
-    laser.y = player.y
+    laser.pos = ship.pos
     laser.vy = -8
     lasers_list.append(laser)
 
 
-def add_meteor():
-    image = random.choice(["meteor1", "meteor2", "meteor3", "meteor4"])
-    meteor = Actor(image)
-    meteor.x = random.randint(20, WIDTH - 20)
-    meteor.y = -10
-    meteor.vy = random.randint(2, 10)
-    meteor.va = random.randint(-5, 5)
-    meteors_list.append(meteor)
+def regenerate_ammo():
+    ship.ammunition += 1
 
 
-def add_ammo():
-    player.ammo += 1
+"""EVENTS"""
 
 
-""" INITIALIZATION """
+def on_mouse_down(pos):
+    if ship.ammunition <= 0:
+        return
+
+    add_laser()
+    sounds.laser.play()
+    ship.ammunition -= 1
+    clock.schedule(regenerate_ammo, 1)
 
 
-def init_lifes():
-    for i in range(player.lifes):
-        life = Actor("life")
-        life.x = life.width / 2 + i * life.width
-        life.y = life.height / 2
-        lifes_list.append(life)
-
+"""INITIALIZATION"""
 
 pygame.mouse.set_visible(False)
-init_lifes()
 
 music.play("space")
 music.set_volume(0.3)
